e2d0ea35e57412c065934d7595eed5ebd59b8eb4
techaryahs <tech.aryahs@gmail.com>
2026-05-12 11:57:59 +0530

    amit

--- backend/index.js
+++ backend/index.js
@@ -93,7 +93,7 @@ module.exports = app;
 
 // Only listen if run directly (useful for local development)
 if (require.main === module) {
-  const PORT = process.env.PORT || 5001;
+  const PORT = process.env.PORT || 5011;
   server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
 
